0d75fa5f2681658bf59bb9c6e0fcf769a7c6dbee
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-04-07 09:24:08 +0200

    Added SpecialitiesService and SpecialityServiceMap Impl. CLoses #31

---
+++ pet-clinic-data/src/main/java/ccbill/training/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package ccbill.training.sfgpetclinic.services;
+
+import ccbill.training.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
